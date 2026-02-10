156dca69d65364fa38afc7726b4222cc0dc5bd6d
RAVELLA VIKHIL <164651332+VikhilRavella@users.noreply.github.com>
2026-02-10 23:24:22 +0530

    Update settings.py

--- agro_platform/agro_platform/settings.py
+++ agro_platform/agro_platform/settings.py
@@ -1,4 +1,12 @@
 from pathlib import Path
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # ================= BASE =================
 BASE_DIR = Path(__file__).resolve().parent.parent
